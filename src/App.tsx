50c92499affe5a2cf662cefa6d89b58df30bb083
cagatay-usta <cagatay_usta@windowslive.com>
2023-08-12 23:08:10 +0300

    setup basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      
+app
+<Outlet />
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home.tsx'
+import About from './Pages/About.tsx'
+import NotFound from './Pages/NotFound.tsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='home' element={<Home />} />
+          <Route path="About" element={<About />} />
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
