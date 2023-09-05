5d33e3c08e5b6492b223db485d75d02be3284ad7
cagatay-usta <cagatay_usta@windowslive.com>
2023-09-05 23:38:38 +0300

    add routing

---
+++ src/Pages/Clothing.tsx
@@ -0,0 +1,8 @@
+
+function Clothing() {
+  return (
+    <div>Clothing</div>
+  )
+}
+
+export default Clothing
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home.tsx";
 import About from "./Pages/About.tsx";
 import NotFound from "./Pages/NotFound.tsx";
-import "./style/global.css"
+import "./style/global.css";
+import Collection from "./Pages/Collection.tsx";
+import Clothing from "./Pages/Clothing.tsx";
+import Cart from "./Pages/Cart.tsx";
+import Product from "./Pages/Product.tsx";
+import Checkout from "./Pages/Checkout.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -14,6 +19,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />}>
           <Route path="home" element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="collection" element={<Collection />} />
+          <Route path="clothing" element={<Clothing />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="product" element={<Product />} />
+          <Route path="checkout" element={<Checkout />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
